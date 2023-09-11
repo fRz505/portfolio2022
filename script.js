c376c13efb09a72369dec64dc9ea35b9d47b6e52
Franco Marquillas <francomarquillas@gmail.com>
2023-09-11 12:55:49 -0300

    changes template

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function responsiveMenu() {
     }
 }
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_v0lvk2q';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar Email';
+      Swal.fire({
+        title: 'Email enviado',
+        icon: 'sucess',
+        confirmButtonText: 'Aceptar'
+      })
+    }, (err) => {
+      btn.value = 'Enviar Email';
+      alert(JSON.stringify(err));
+    });
+});
+
