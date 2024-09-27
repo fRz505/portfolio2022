913bc31a9974fa7f5c8acb0aa03fd28ee973b2f2
Franco <francomarquillas@gmail.com>
2024-09-27 13:47:28 -0300

    changes html

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 function responsiveMenu() {
-    
-    var nav = document.getElementById("nav");
-    if(nav.className==="responsive"){
-        nav.className = "nav"
-    }else{
-        nav.className = "responsive";
-    }
+  var nav = document.getElementById("nav");
+  if (nav.className === "responsive") {
+    nav.className = "nav";
+  } else {
+    nav.className = "responsive";
+  }
 }
 
-const btn = document.getElementById('button');
+const btn = document.getElementById("button");
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-   btn.value = 'Sending...';
+  btn.value = "Sending...";
 
-   const serviceID = 'default_service';
-   const templateID = 'template_v0lvk2q';
+  const serviceID = "default_service";
+  const templateID = "template_v0lvk2q";
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Enviar Email';
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Enviar Email";
       Swal.fire({
-        title: 'Email enviado',
-        icon: 'sucess',
-        confirmButtonText: 'Aceptar'
-      })
-    }, (err) => {
-      btn.value = 'Enviar Email';
+        title: "Email enviado",
+        icon: "sucess",
+        confirmButtonText: "Aceptar",
+      });
+    },
+    (err) => {
+      btn.value = "Enviar Email";
       alert(JSON.stringify(err));
-    });
+    }
+  );
 });
-
